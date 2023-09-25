14e72755be8c889b47765cc29ac65a8079f48ffc
César Guadarrama <cesar@ocho.co>
2023-09-25 13:40:30 -0600

    Fixes

--- App.js
+++ App.js
@@ -1,7 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import * as Sentry from "sentry-expo";
+
+Sentry.init({
+  dsn: "https://0bd57d8546e7b69bf4bfa061ed549eed@o689491.ingest.sentry.io/4505942760620032",
+  enableInExpoDevelopment: true,
+  debug: true, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
+});
 
 export default function App() {
+  useEffect(() => {
+    Sentry.Native.captureEvent({
+      message: "My first Sentry native event!",
+      level: Sentry.Severity.Info,
+    });
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -13,8 +28,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
